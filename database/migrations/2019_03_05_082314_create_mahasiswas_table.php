89541ce845a4e638b336df8072a6e2b8521f0236
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2019-03-05 15:42:52 +0700

    add database migration

---
+++ database/migrations/2019_03_05_082314_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+//            $table->bigIncrements('id');
+            $table->string('nim')->primary();
+            $table->string('nama');
+            $table->string('email');
+            $table->string('no_hp', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
