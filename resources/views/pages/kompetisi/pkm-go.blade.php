e063838d04c25b094f628644f683eeaacf242416
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2019-03-27 09:56:50 +0700

    fix

--- resources/views/pages/kompetisi/pkm-go.blade.php
+++ resources/views/pages/kompetisi/pkm-go.blade.php
@@ -37,13 +37,6 @@
                     and scrambled it to make a type specimen book.
                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining
                     essentially unchanged. </p>
-                <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-                    Ipsum.</p>
-                <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the
-                    more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of
-                    the word in classical literature, discovered the
-                    undoubtable source.</p>
                 <p>
                 <p><b>Rulebook : </b></p>
                 <a href="{{ asset('assets/rulebook/pkm/PKM-AI.pdf') }}" class="btn btn-theme">PKM-AI</a>
